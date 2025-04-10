111c68e99f6dc637637728d6e2d00da1d74bc6f5
Riwandi <44519384+rwndy@users.noreply.github.com>
2025-04-10 12:50:25 +0700

    Time: 286 ms (7.69%) | Memory: 64.7 MB (7.69%) - LeetSync

---
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.ts
@@ -0,0 +1,52 @@
+function minOperations(nums: number[], k: number): number {
+
+    if (nums.some(num => num < k)) {
+        return -1;
+    }
+
+    if (nums.every(num => num === k)) {
+        return 0;
+    }
+    
+    let operations = 0;
+
+    let current = [...nums];
+    
+    while (!current.every(num => num === k)) {
+        const greaterThanK = current.filter(num => num > k);
+        if (greaterThanK.length > 0 && greaterThanK.every(val => val === greaterThanK[0])) {
+        
+            current = current.map(num => num > k ? k : num);
+            operations++;
+            continue;
+        }
+        
+        let validHFound = false;
+        
+        const uniqueValues = [...new Set(current)].sort((a, b) => a - b);
+        
+        for (let i = uniqueValues.length - 1; i >= 0; i--) {
+            if (uniqueValues[i] <= k) continue; // Skip values <= k
+            
+            for (let j = k; j < uniqueValues[i]; j++) {
+           
+                const greaterThanJ = current.filter(num => num > j);
+         
+                if (greaterThanJ.length > 0 && greaterThanJ.every(val => val === greaterThanJ[0])) {
+                    current = current.map(num => num > j ? j : num);
+                    operations++;
+                    validHFound = true;
+                    break;
+                }
+            }
+            
+            if (validHFound) break;
+        }
+        
+        if (!validHFound) {
+            return -1;
+        }
+    }
+    
+    return operations;
+};
