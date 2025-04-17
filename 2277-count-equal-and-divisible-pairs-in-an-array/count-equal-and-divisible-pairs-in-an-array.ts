0f9e1b8f85480264bf06d30effa492c6a7aa0622
Riwandi <44519384+rwndy@users.noreply.github.com>
2025-04-17 11:34:30 +0700

    Time: 4 ms (62.50%) | Memory: 58.6 MB (16.67%) - LeetSync

---
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.ts
@@ -0,0 +1,31 @@
+/*
+masalahnya di element array ini adakah element yg sama?
+
+
+*/
+
+function countPairs(nums: number[], k: number): number {
+    const valueIndices: Map<number, number[]> = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if (!valueIndices.has(num)) {
+            valueIndices.set(num, []);
+        }
+        valueIndices.get(num)!.push(i);
+    }
+    
+    let count = 0;
+    
+    for (const indices of valueIndices.values()) {
+    
+        for (let i = 0; i < indices.length; i++) {
+            for (let j = i + 1; j < indices.length; j++) {
+                if ((indices[i] * indices[j]) % k === 0) {
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return count;
+};
