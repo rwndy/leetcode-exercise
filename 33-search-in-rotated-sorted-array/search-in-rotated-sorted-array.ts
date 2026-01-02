da5a13f967ed9ebced6119b2e6cefc6a667e7df1
Riwandi <44519384+rwndy@users.noreply.github.com>
2026-01-02 22:48:36 +0700

    Time: 0 ms (100.00%) | Memory: 55.4 MB (65.43%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
@@ -0,0 +1,25 @@
+function search(nums: number[], target: number): number {
+    let low: number = 0, high:number = nums.length - 1
+
+    while (low <= high) {
+        const mid = Math.floor((low + high) / 2)
+
+        if (nums[mid] === target) return mid
+
+        if (nums[low] <= nums[mid]) {
+            if (nums[low] <= target && target < nums[mid]) {
+                high = mid - 1
+            } else {
+                low = mid + 1
+            }
+        } else {
+            if (nums[mid] < target && target <= nums[high]) {
+                low = mid + 1
+            } else {
+                high = mid - 1
+            }
+        }
+    }
+
+    return -1
+};
