b0bf9316659709d6689e7864cb132cf7c6e10ad4
Riwandi <44519384+rwndy@users.noreply.github.com>
2026-01-16 01:09:59 +0700

    Time: 0 ms (100.00%) | Memory: 60.7 MB (72.20%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isBalanced(root: TreeNode | null): boolean {
+    function checkHeight(node: TreeNode | null): number {
+        if (node === null) return 0
+
+        const leftHeight = checkHeight(node.left)
+        if (leftHeight === -1) return -1
+
+        const rightHeight = checkHeight(node.right)
+        if (rightHeight === -1) return -1
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1
+    }
+
+    return checkHeight(root) !== -1
+};
