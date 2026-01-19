bf363fa5b08d0e4e01c30dc7f3ab8bb2e969830e
Riwandi <44519384+rwndy@users.noreply.github.com>
2026-01-19 16:22:59 +0700

    Time: 0 ms (100.00%) | Memory: 55.2 MB (73.65%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function postorderTraversal(root: TreeNode | null): number[] {
+    let result: number[] = []
+   
+    function Root(node: TreeNode | null, res ): void {
+        if (node === null) return
+        
+
+        Root(node?.left, res)
+        Root(node?.right, res)
+
+        res.push(node?.val)
+    }
+
+    Root(root, result)
+    return result
+};
