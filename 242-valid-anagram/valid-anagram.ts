8a0949b5de00eab694b09b51fc836f3f2d1f8719
Riwandi <44519384+rwndy@users.noreply.github.com>
2026-01-23 20:32:12 +0700

    Time: 16 ms (43.05%) | Memory: 61.7 MB (17.35%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.ts
@@ -0,0 +1,18 @@
+function isAnagram(s: string, t: string): boolean {
+
+    if (s.length !== t.length) return false
+    const charCount = {}
+
+    for (const char of s) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    for (const char of t) {
+        if (!charCount[char]) {
+            return false;
+        }
+        charCount[char]--;
+    }
+
+    return true
+};
