e41d0256f475b20cbd892c545e2adbc4e6db5b39
Riwandi <44519384+rwndy@users.noreply.github.com>
2026-03-18 16:38:02 +0700

    Time: 2 ms (72.53%) | Memory: 57.5 MB (38.24%) - LeetSync

---
+++ 1-two-sum/two-sum.ts
@@ -0,0 +1,13 @@
+function twoSum(nums: number[], target: number): number[] {
+   const hashMap = new Map()
+    
+    for (let i = 0; i < nums.length; ++i) {
+        const complement = target - nums[i]
+
+        if (hashMap.has(complement)) {
+            return [hashMap.get(complement), i]
+        }
+
+        hashMap.set(nums[i], i)
+    }
+};
