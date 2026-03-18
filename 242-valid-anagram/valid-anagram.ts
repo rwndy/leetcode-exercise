561eb6af8d3470091e1c8c3fb9d9aa08359fb8cd
Riwandi <44519384+rwndy@users.noreply.github.com>
2026-03-18 17:23:32 +0700

    Time: 28 ms (15.83%) | Memory: 59.1 MB (52.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.ts
+++ 242-valid-anagram/valid-anagram.ts
@@ -1,21 +1,5 @@
 function isAnagram(s: string, t: string): boolean {
+if (s.length !== t.length) return false;
 
-    if (s.length !== t.length) return false
-    const charCount = new Map()
-
-    for (const char of s) {
-        charCount.set(char, (charCount.get(char) || 0) + 1);
-    }
-
-    for (const char of t) {
-        const count = charCount.get(char);
-        
-        if (!count) {
-            return false;
-        }
-        
-        charCount.set(char, count - 1);
-    }
-
-    return true
+return s.split('').sort().join('') === t.split('').sort().join('');
 };
