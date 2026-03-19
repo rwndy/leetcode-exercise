3e960f0331c3ff1803e8cb0c5b8df04d498e4e09
Riwandi <44519384+rwndy@users.noreply.github.com>
2026-03-19 16:44:29 +0700

    Time: 62 ms (83.22%) | Memory: 67.2 MB (57.47%) - LeetSync

---
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
+    if (!headA || !headB) return null;
+
+    let pointerA: ListNode | null = headA;
+    let pointerB: ListNode | null = headB;
+
+    while (pointerA !== pointerB) {
+        pointerA = pointerA ? pointerA.next : headB;
+        pointerB = pointerB ? pointerB.next : headA;
+    }
+
+    return pointerA;
+};
