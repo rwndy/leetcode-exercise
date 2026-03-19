a2dd744800910df7e8a8f68d1c45c195ea061ae1
Riwandi <44519384+rwndy@users.noreply.github.com>
2026-03-19 22:41:15 +0700

    Time: 18 ms (97.20%) | Memory: 68.7 MB (48.77%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.ts
@@ -0,0 +1,15 @@
+function groupAnagrams(strs: string[]): string[][] {
+    const map = new Map()
+
+    for (const s of strs ) {
+       const sorted = s.split('').sort().join()
+
+        if (!map.has(sorted)) {
+            map.set(sorted, [])
+        }
+
+        map.get(sorted)!.push(s)
+    }
+
+    return Array.from(map.values())
+};
