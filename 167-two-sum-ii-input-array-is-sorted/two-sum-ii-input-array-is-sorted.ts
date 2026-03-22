f9affda60339b38a4cb8b0a4c5a5e0b734333430
Riwandi <44519384+rwndy@users.noreply.github.com>
2026-03-22 16:19:35 +0700

    Time: 1 ms (81.78%) | Memory: 55.8 MB (93.03%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
@@ -2,19 +2,15 @@ function twoSum(numbers: number[], target: number): number[] {
     let left = 0
     let right = numbers.length - 1
 
-    while (left <= right) {
+    while (left < right) {
         const sum = numbers[left] + numbers[right] // 2 + 15, 7 + 11, 2+7,
-
-        if (sum < target) {
-            left++
-        }
-
-        if (sum > target) {
-            right--
-        }
         
         if (sum === target) {
             return [left + 1, right + 1]
+        } else if (sum < target) {
+            left++
+        } else if (sum > target) {
+            right--
         }
     }
 };
